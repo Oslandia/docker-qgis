381b5e0f4d39b231402777d658183a75aa370e0d
Éric Lemoine <eric.lemoine@oslandia.com>
2018-03-13 20:27:26 +0100

    build: pass --post-clean to dpkg-buildpackage

--- qgis-build/build.sh
+++ qgis-build/build.sh
@@ -4,6 +4,6 @@ set -e
 
 cd /qgis/QGIS
 dch -l ~${DIST} --force-distribution --distribution ${DIST} "${DIST} build"
-DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -us -uc -b --pre-clean
+DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -us -uc -b --pre-clean --post-clean
 
 exit 0
