d583464c15b9a4eb1c13ae76b7ef8f734dbbd0ea
Éric Lemoine <eric.lemoine@oslandia.com>
2018-06-08 10:39:08 +0200

    exec: do not use multiwatch when PROCESSES ≤ 1

--- qgis-exec/cmd.sh
+++ qgis-exec/cmd.sh
@@ -2,4 +2,10 @@
 
 [[ $DEBUG == "1" ]] && env
 
-exec /usr/bin/xvfb-run /usr/bin/spawn-fcgi -p 5555 -n -d /home/qgis -- /usr/bin/multiwatch -f $PROCESSES -- /usr/lib/cgi-bin/qgis_mapserv.fcgi
+if [[ -n $PROCESSES && $PROCESSES -gt 1 ]]; then
+    MULTIWATCH="/usr/bin/multiwatch -f $PROCESSES --"
+else
+    MULTIWATCH=""
+fi
+
+exec /usr/bin/xvfb-run /usr/bin/spawn-fcgi -p 5555 -n -d /home/qgis -- $MULTIWATCH /usr/lib/cgi-bin/qgis_mapserv.fcgi
