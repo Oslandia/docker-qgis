214828ed624357ff8d854617348942018ee7405f
Éric Lemoine <eric.lemoine@oslandia.com>
2019-10-06 10:23:49 +0200

    [qgis-exec] Remove multiwatch
    Remove multiwatch, it has proved buggy.

--- qgis-exec/cmd.sh
+++ qgis-exec/cmd.sh
@@ -2,10 +2,4 @@
 
 [[ $DEBUG == "1" ]] && env
 
-if [[ -n $PROCESSES && $PROCESSES -gt 1 ]]; then
-    MULTIWATCH="/usr/bin/multiwatch -f $PROCESSES --"
-else
-    MULTIWATCH=""
-fi
-
-exec /usr/bin/xvfb-run --auto-servernum --server-num=1 /usr/bin/spawn-fcgi -p 5555 -n -d /home/qgis -- $MULTIWATCH /usr/lib/cgi-bin/qgis_mapserv.fcgi
+exec /usr/bin/xvfb-run --auto-servernum --server-num=1 /usr/bin/spawn-fcgi -p 5555 -n -d /home/qgis -- /usr/lib/cgi-bin/qgis_mapserv.fcgi
